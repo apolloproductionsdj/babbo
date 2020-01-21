2bc3d76010bc7949b9c0a75e54c97316bf14c347
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-21 15:50:16 +0000

    added user show page, routes to it and nav-item in navigation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :menus, only: [:show]
+
+  resources :users, only: :show
   
 end
